6009ef62c2f8ef020c5a82f8ab2cba72f1f91e73
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-22 15:00:01 +0330

    start Project

---
+++ CardWorkout-programmatic/CWButton.swift
@@ -0,0 +1,12 @@
+//
+//  CWButton.swift
+//  CardWorkout-programmatic
+//
+//  Created by Peyman on 12/22/24.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+}
---
+++ CardWorkout-programmatic/CardSelectionVC.swift
@@ -0,0 +1,20 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-programmatic
+//
+//  Created by Peyman on 12/22/24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+    
+
+
+}

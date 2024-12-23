e2d952cb4aa86925e468c217540efecd07511084
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-23 17:07:14 +0330

    add logic to button and systemImage

--- CardWorkout-programmatic/CWButton.swift
+++ CardWorkout-programmatic/CWButton.swift
@@ -11,7 +11,7 @@ class CWButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+       
     }
     
     
@@ -19,20 +19,19 @@ class CWButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(backgroundColor: UIColor, title: String)
+    init(backgroundColor: UIColor, title: String, systemImageName: String)
     {
         super.init(frame: .zero)
-        self.backgroundColor = backgroundColor
-        setTitle(title, for: .normal)
-        configure()
-    }
-    
-    
-    func configure ()
-    {
-        layer.cornerRadius = 8
-        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
-        setTitleColor(.white, for: .normal)
+        configuration = .tinted()
+        configuration?.title = title
+        configuration?.baseForegroundColor = backgroundColor
+        configuration?.baseBackgroundColor = backgroundColor
+        configuration?.cornerStyle = .medium
+        
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 5
+        configuration?.imagePlacement = .leading
         translatesAutoresizingMaskIntoConstraints = false
     }
+    
 }
